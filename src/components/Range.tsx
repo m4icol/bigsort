d15066299fd6b6b91c78c81309f28d055f2a3c55
Maickol <m4icol.r12@gmail.com>
2025-06-10 11:36:09 -0500

    refactor: max value (20 to 15) in range

--- src/components/Range.tsx
+++ src/components/Range.tsx
@@ -9,7 +9,7 @@ type RangeProps = {
         aria-label="Select the number of elements"
         type="range"
         min={5}
-        max={20}
+        max={15}
         value={value}
         onChange={(e) => onChange(Number(e.target.value))}
         className="w-full accent-BM-border h-1"
