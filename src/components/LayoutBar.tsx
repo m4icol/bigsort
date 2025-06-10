a7ddba41bb5271d789248bf04933bcf9d939f1a0
Maickol <m4icol.r12@gmail.com>
2025-06-10 10:58:55 -0500

    feat: value layoutbars added

--- src/components/LayoutBar.tsx
+++ src/components/LayoutBar.tsx
@@ -16,19 +16,20 @@ function LayoutBar({level = 1, index, isActive, actionType }: LayoutBarProps) {
 
     const calculateHeight = (level: number): number => {
         const heightLevel = Math.max(0, Math.min(15, level));
-        return Math.round((heightLevel) * (310 / 14));
+        return Math.round((heightLevel) * (300 / 14));
     };
 
     const height = calculateHeight(level);
 
     return (
-        <div className="flex flex-col gap-2 items-center w-full lg:w-10">
+        <div className="flex flex-col gap-3 items-center w-full lg:w-10">
             <div 
-            className={`rounded-md border-1 w-full border-BM-border flex-shrink-0 ${getBarColor()}`}
-            style={{ height: `${height}px` }}
-            >
+                className={`rounded-md border-1 w-full border-BM-border flex-shrink-0 flex items-end pb-1 pt-5.5 ${getBarColor()}`}
+                style={{ height: `${height}px` }}>
+                <p className="text-BM-border font-semibold text-xs text-center w-full">{level}</p>
             </div>
-            <p className="text-BM-subtext text-xs text-center w-full">{index}</p>
+
+            <p className="text-BM-subtext text-sm text-center w-full">{index}</p>
         </div>
     );
 }
